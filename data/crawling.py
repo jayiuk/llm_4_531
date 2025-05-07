0ac3e3b26ccca39889e4e3543993aeb91278597e
jayiuk <jayiuk987@gmail.com>
2025-05-07 20:34:24 +0900

    get data

--- data/crawling.py
+++ data/crawling.py
@@ -2,6 +2,8 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import json
+import re
 
 driver = webdriver.Chrome()
 
@@ -21,3 +23,13 @@ def crawling_tistory(url):
         driver.quit()
         
     return contents
+
+def data_parse(data):
+    pattern = r'(?P<heading>\d{1,2}\. .+?)\n(?P<content>.*?)(?=\n\d{1,2}\. |\Z)'
+    matches = re.finditer(pattern, data, re.DOTALL)
+    new_data = []
+    for match in matches:
+        title = match.group('heading').strip()
+        contents = match.group('content').strip()
+        new_data.append({'title' : title, 'content' : contents})
+    return new_data
--- data/crawling_test.py
+++ data/crawling_test.py
@@ -1,4 +1,6 @@
 from crawling import crawling_tistory
+from crawling import data_parse
 base_url = 'https://hse30.tistory.com/798'
 con = crawling_tistory(base_url)
-print(con)
+real_con = data_parse(con)
+print(real_con)
