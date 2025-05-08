69531f46f4ec75c7be78bd3f4404d8ee531a32d9
jayiuk <jayiuk987@gmail.com>
2025-05-08 14:28:01 +0900

    get data json

--- data/getdata.py
+++ data/getdata.py
@@ -1,7 +1,7 @@
 import json
 from collections import OrderedDict
 
-def get_data_531_basic(prompt, rm):
+def get_data_531_basic(prompt, rm, path):
     try:
         instruction = "사용자의 1RM 정보를 바탕으로 기본적인 5/3/1 루틴을 생성하세요."
         tm = rm * 0.9
@@ -23,12 +23,14 @@ def get_data_531_basic(prompt, rm):
             routine[week] = []
             for pct, rep in zip(percentages[week], reps[week]):
                 weight = round((tm * pct) / 2.5) * 2.5
-                routine[week].append(f"{weight:.1f}kg X {rep}")
+                routine[week].append(f"루틴을 만들어드리겠습니다. {weight:.1f}kg X {rep} 이런 식으로 하시면 됩니다. 요일은 편하신 날에 하시되, 주3일은 꼭 하시기 바랍니다.")
         
         data = OrderedDict()
         data["instruction"] = instruction
         data["input"] = prompt
         data["output"] = routine
+        with open(path, 'w') as f:
+            json.dump(data, f)
     
     except TypeError:
         instruction = "사용자의 1RM 정보를 바탕으로 기본적인 5/3/1 루틴을 생성하세요."
@@ -54,15 +56,18 @@ def get_data_531_basic(prompt, rm):
             for r in tms:
                 for pct, rep in zip(percentages[week], reps[week]):
                     weight = round((r * pct) / 2.5) * 2.5
-                    routine[week].append(f"{weight:.1f}kg X {rep}")
+                    routine[week].append(f"루틴을 만들어드리겠습니다. {weight:.1f}kg X {rep} 이런 식으로 하시면 됩니다. 요일은 편하신 날에 하시되, 주 3일은 꼭 하시기 바랍니다.")
         
         data = OrderedDict()
         data["instruction"] = instruction
         data["input"] = prompt
         data["output"] = routine
 
+        with open(path, 'w') as f:
+            json.dump(data, f)
+
 
     return data
 
-def get_data_531_7week(prompt, rm):
+def get_data_531_7week(prompt, rm, path):
     tm = rm * 0.9
